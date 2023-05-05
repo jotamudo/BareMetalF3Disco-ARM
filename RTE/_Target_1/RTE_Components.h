f347c6daaaad6106a21764d94a53eb3b18da8264
j_mudo <juanvasconceloslopes@gmail.com>
2023-05-04 21:25:51 -0300

    Initial Commit

---
+++ RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,23 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'bare-metal-LL' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f3xx.h"
+
+/* Arm-Packs::Test:Unity:Classic:2.5.0 */
+#define RTE_Test_Unity_Classic        /* Unity Classic */
+
+
+#endif /* RTE_COMPONENTS_H */
